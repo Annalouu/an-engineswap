c540678c7c2e94dfcd6afc3242d34436c514758e
reyyghi <reyhafizh07@gmail.com>
2023-12-07 17:29:00 +0700

    created data file (installed_sound, location, sound)

---
+++ data/installed_sound.lua
@@ -0,0 +1,11 @@
+return {
+	["29UBK581"] = {
+	    exhaust = "aq2jzgterace",
+	    category = "car",
+	},
+	["24JMU437"] = {
+	    exhaust = "f10m5",
+	    category = "car",
+	},
+
+}
---
+++ data/location.lua
@@ -0,0 +1,32 @@
+return {
+    {
+        coords = vector3(1172.31, 2661.94, 37.99),
+        radius = 2.0,
+        groups = {['mechanic'] = 1},
+        debug = false,
+        drawtext = {
+            inveh = "Press E to engineswap",
+            outveh = "You need to be in a vehicle!"
+        }
+    },
+    {
+        coords = vector3(-367.13, -124.73, 38.7),
+        radius = 2.0,
+        groups = {"mechanic"},
+        debug = false,
+        drawtext = {
+            inveh = "Press E to engineswap",
+            outveh = "You need to be in a vehicle!"
+        }
+    },
+    {
+        coords = vector3(-1423.39, -451.31, 35.91),
+        radius = 2.0,
+        groups = "mechanic",
+        debug = false,
+        drawtext = {
+            inveh = "Press E to engineswap",
+            outveh = "You need to be in a vehicle!"
+        }
+    }
+}
