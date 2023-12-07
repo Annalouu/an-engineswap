88f8adedb303161cd61f2c63bce11fc5d7e39670
reyyghi <reyhafizh07@gmail.com>
2023-12-07 17:26:29 +0700

    added bridge

---
+++ bridge/qb.lua
@@ -0,0 +1,51 @@
+if GetResourceState("qb-core") ~= "started" then return end 
+
+local PlayerData = {}
+local isServer = IsDuplicityVersion()
+local QBCore = exports['qb-core']:GetCoreObject()
+
+Core = {}
+
+function Core.getMyJob ( type )
+    if type == "name" then
+        return QBCore.Functions.GetPlayerData().job.name
+    elseif type == "grade" then
+        return QBCore.Functions.GetPlayerData().job.grade.level
+    end
+end
+
+if isServer then
+    function Core.getPlayer ( source )
+        PlayerData[source] = QBCore.Functions.GetPlayer(source)
+        return PlayerData[source]
+    end
+
+    function Core.removeMoney(source, type, amount )
+        local Player = PlayerData[source]
+        if not Player then return false end
+        return Player.Functions.RemoveMoney(type, amount)
+    end
+
+    function Core.getCid( source )
+        local Player = PlayerData[source]
+        if not Player then return "Unknown" end
+        return Player.PlayerData.citizenid
+    end
+
+    function Core.getName( source )
+        local Player = PlayerData[source]
+        if not Player then return "Unknown" end
+        return ("%s %s"):format(Player.PlayerData.charinfo.firstname, Player.PlayerData.charinfo.lastname)
+    end
+
+    function Core.getMyJob ( source, type )
+        local Player = PlayerData[source]
+        if not Player then return "Unknown" end
+
+        if type == "name" then
+            return Player.PlayerData.job.name
+        elseif type == "grade" then
+            return Player.PlayerData.job.grade.level
+        end
+    end
+end
