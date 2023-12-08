af4374639516843b69cd9dcf888d1ab670b7fe0d
reyyghi <reyhafizh07@gmail.com>
2023-12-08 13:56:54 +0700

    update config

--- shared/config.lua
+++ shared/config.lua
@@ -1,5 +1,6 @@
 Config = {
-    usePayments = true,
-    RenewedBanking = false,
-    moneyType = "cash"
+    updateInterval = 5, --- time interval for storing data (minute)
+    usePayments = true, --- false if you don't want to use payment
+    RenewedBanking = false, --- true if you use renewed banking
+    moneyType = "cash" --- payment method (bank or cash)
 }
