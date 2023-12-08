b03c3e05e5276dd04b1df5010b872ba804856da4
reyyghi <reyhafizh07@gmail.com>
2023-12-08 13:56:10 +0700

    update data file

--- data/installed_sound.lua
+++ data/installed_sound.lua
@@ -1,10 +1,14 @@
 return {
 	["29UBK581"] = {
-	    exhaust = "aq2jzgterace",
+	    exhaust = "ta488f154",
 	    category = "car",
 	},
 	["24JMU437"] = {
-	    exhaust = "f10m5",
+	    exhaust = "npolchar",
+	    category = "car",
+	},
+	["01YJU560"] = {
+	    exhaust = "porsche57v10",
 	    category = "car",
 	},
 
--- data/location.lua
+++ data/location.lua
@@ -1,32 +1,10 @@
 return {
-    {
-        coords = vector3(1172.31, 2661.94, 37.99),
-        radius = 2.0,
-        groups = {['mechanic'] = 1},
-        debug = false,
-        drawtext = {
-            inveh = "Press E to engineswap",
-            outveh = "You need to be in a vehicle!"
-        }
-    },
-    {
-        coords = vector3(-367.13, -124.73, 38.7),
-        radius = 2.0,
-        groups = {"mechanic"},
-        debug = false,
-        drawtext = {
-            inveh = "Press E to engineswap",
-            outveh = "You need to be in a vehicle!"
-        }
-    },
-    {
-        coords = vector3(-1423.39, -451.31, 35.91),
-        radius = 2.0,
-        groups = "mechanic",
-        debug = false,
-        drawtext = {
-            inveh = "Press E to engineswap",
-            outveh = "You need to be in a vehicle!"
-        }
-    }
+	{
+		coords = vector3(1174.906616, 2640.307373, 36.753799),
+		radius = 2.800000,
+		groups = "mechanic",
+		debug = false,
+		drawtext = { inveh = "Press E to engineswap", outveh = "You need to be in a vehicle!" }
+	},
+
 }
