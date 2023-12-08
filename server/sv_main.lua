b36151d451df74b4a624e7a97af3ee75ed6f1a89
reyyghi <reyhafizh07@gmail.com>
2023-12-08 13:56:37 +0700

    update sv_main

--- server/sv_main.lua
+++ server/sv_main.lua
@@ -1,55 +1,56 @@
 local Swap = {}
 
 local Installed_Sound = require "data.installed_sound"
-
-local function SaveExhaust(FileData)
-  local result = {}
-  for plate, data in pairs(FileData) do
-      if FileData[plate] then
-        local plate_ = string.gsub(plate, '^%s*(.-)%s*$', '%1')
-        result[#result + 1] = ('\t["%s"] = {\n\t    exhaust = "%s",\n\t    category = "%s",\n\t},\n'):format(
-        plate_, data.exhaust, data.category)
-      end
-  end
-
-  local DataTable = ('return {\n%s\n}'):format(table.concat(result, ""))
-  SaveResourceFile(GetCurrentResourceName(), 'data/installed_sound.lua', DataTable, -1)
-end
+local Locations = require "data.location"
+local Sound = require "data.sound"
 
 RegisterServerEvent("an-engine:server:engine", function(data)
-  if GetInvokingResource() then return end
-  local src = source
-  local Player = Core.getPlayer(src)
-  if not Player then return end
-  local cid = Core.getCid(src)
-  local myName = Core.getName(src)
-  local veh = GetVehiclePedIsIn(GetPlayerPed(src), false)
+    if GetInvokingResource() then return end
+    local src = source
+    local Player = Core.getPlayer(src)
+    if not Player then return end
+    local cid = Core.getCid(src)
+    local myName = Core.getName(src)
+    local veh = GetVehiclePedIsIn(GetPlayerPed(src), false)
 
-  local category = data.category
-  local engine = data.sound
-  local price = data.price
-  local job = data.job
+    if not veh then return end
 
-  if Config.usePayments then
-    local moneyType = Config.moneyType
-    if not Core.removeMoney(src, moneyType, price) then return
-      Utils.Notify({source = src, msg = "You dont have enough money..", type = "error"}) 
-    end
-    
-    if Config.RenewedBanking then
-      exports['Renewed-Banking']:handleTransaction(cid, "Engine local Swap", price, "Swapped Engine by Mechanics", "Los Santos Customs", myName, "withdraw")
-      if job then
-        local myJob = Core.getMyJob(src, "name")
-        exports['Renewed-Banking']:addAccountMoney(myJob, price)
-      end
-    end
-  end
- 
-  if engine ~= nil and veh ~= 0 then
     local plate = GetVehicleNumberPlateText(veh)
+    local ent = Entity(veh).state
+    local category = data.category
+    local engine = data.sound
+    local price = data.price
+    local job = data.job
 
-    if Swap[plate] == nil then
-      Swap[plate] = {}
+    if data.setDefualt then  
+        ent.exhaust = nil
+        ent.engine = nil
+        Swap[plate] = nil
+
+        Installed_Sound[plate] = nil
+        return
+    end
+
+    if not engine then return end
+
+    if Config.usePayments then
+        local moneyType = Config.moneyType
+        if not Core.removeMoney(src, moneyType, price) then return
+            Utils.Notify({source = src, msg = "You dont have enough money..", type = "error"}) 
+        end
+        
+        if Config.RenewedBanking then
+            exports['Renewed-Banking']:handleTransaction(cid, "Engine local Swap", price, "Swapped Engine by Mechanics", "Los Santos Customs", myName, "withdraw")
+
+            if job then
+                local myJob = Core.getMyJob(src, "name")
+                exports['Renewed-Banking']:addAccountMoney(myJob, price)
+            end
+        end
+    end
+ 
+    if not Swap[plate] then
+        Swap[plate] = {}
     end
 
     Swap[plate].current = Swap[plate].exhaust or engine
@@ -58,46 +59,69 @@ RegisterServerEvent("an-engine:server:engine", function(data)
     Swap[plate].engine = engine
     Swap[plate].category = category
 
-    local ent = Entity(veh).state
-
     ent.exhaust = Swap[plate].exhaust
     ent.engine = Swap[plate].engine
 
     Installed_Sound[plate] = {
-      exhaust = Swap[plate].exhaust,
-      category = Swap[plate].category
+        exhaust = Swap[plate].exhaust,
+        category = Swap[plate].category
     }
-
-    SaveExhaust(Installed_Sound)
-  end
 end)
 
 CreateThread(function()
-  for plate, data in pairs(Installed_Sound) do
-    Swap[plate] = data
-    Swap[plate].engine = data.exhaust
-    Swap[plate].current = data.exhaust
-  end
-
-  for _,v in ipairs(GetAllVehicles()) do
-    local plate = GetVehicleNumberPlateText(v)
-    if Swap[plate] and plate == Swap[plate].plate then
-      local ent = Entity(v).state
-      ent.exhaust = Swap[plate].exhaust
-      ent.engine = Swap[plate].engine
+    for plate, data in pairs(Installed_Sound) do
+        Swap[plate] = data
+        Swap[plate].engine = data.exhaust
+        Swap[plate].current = data.exhaust
     end
-  end
+
+    for _,v in ipairs(GetAllVehicles()) do
+        local plate = GetVehicleNumberPlateText(v)
+        if Swap[plate] and plate == Swap[plate].plate then
+            local ent = Entity(v).state
+            ent.exhaust = Swap[plate].exhaust
+            ent.engine = Swap[plate].engine
+        end
+    end
+end)
+
+RegisterNetEvent('an-engineswap:server:loadData', function( Player )
+    TriggerClientEvent("an-engineswap:client:loadData", Player, {
+        sound = Sound,
+        zone = Locations
+    })
+end)
+
+RegisterNetEvent('an-engineswap:server:saveZoneData', function(data, type)
+    if type == "add" then
+        Locations[#Locations+1] = data
+    elseif type == "update" then
+        Locations = data
+    end
+
+    SaveFileData(Locations, "location", "zone")
+    TriggerEvent('an-engineswap:server:loadData', -1)
+    lib.print.info("Created New Zone")
 end)
 
 AddEventHandler('entityCreated', function(entity)
   local entity = entity
   Wait(1000)
-  if DoesEntityExist(entity) and GetEntityPopulationType(entity) == 7 and GetEntityType(entity) == 2 then
-    local plate = GetVehicleNumberPlateText(entity)
-    if Swap[plate] and Swap[plate].exhaust then
-      local ent = Entity(entity).state
-      ent.exhaust = Swap[plate].exhaust
-      ent.engine = Swap[plate].engine
+    if DoesEntityExist(entity) and GetEntityPopulationType(entity) == 7 and GetEntityType(entity) == 2 then
+        local plate = GetVehicleNumberPlateText(entity)
+        if Swap[plate] and Swap[plate].exhaust then
+            local ent = Entity(entity).state
+            ent.exhaust = Swap[plate].exhaust
+            ent.engine = Swap[plate].engine
+        end
     end
-  end
+end)
+
+AddEventHandler('txAdmin:events:serverShuttingDown', function() 
+    SaveFileData(Installed_Sound, "installed_sound", "install")
+end)
+
+lib.cron.new(("*/%s * * * *"):format(Config.updateInterval), function ()
+    SaveFileData(Installed_Sound, "installed_sound", "install")
+    lib.print.info("Saved Data File")
 end)
