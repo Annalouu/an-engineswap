ab94378cfdd8f11febd901f1287fa3710ab8c881
reyyghi <reyhafizh07@gmail.com>
2023-12-10 02:02:39 +0700

    create a new command to add sound

--- server/sv_command.lua
+++ server/sv_command.lua
@@ -11,3 +11,17 @@ lib.addCommand('zonelist', {
 }, function(source, args, raw)
     TriggerClientEvent("an-engineswap:client:listCreatedZone", source)
 end)
+
+lib.addCommand('addsound', {
+    help = 'add new sounds to swap engine list',
+    restricted = 'group.admin'
+}, function(source, args, raw)
+    TriggerClientEvent("an-engineswap:client:addSound", source)
+end)
+
+lib.addCommand('soundlist', {
+    help = 'list all engine swap zones',
+    restricted = 'group.admin'
+}, function(source, args, raw)
+    TriggerClientEvent("an-engineswap:client:listAllSound", source)
+end)
