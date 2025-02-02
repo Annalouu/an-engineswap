aa95e959e53753c0c17388e5f31fc41d3e738f4e
Reyghita Hafizh <reyhafizh07@gmail.com>
2025-02-02 17:50:30 +0700

    Update installed_sound.lua

--- data/installed_sound.lua
+++ data/installed_sound.lua
@@ -1,3 +1,18 @@
 return {
-	
+	["83ZMR358"] = {
+	    exhaust = "taaud40v8",
+	    category = "car",
+	},
+	["65JLE315"] = {
+	    exhaust = "ta488f154",
+	    category = "car",
+	},
+	["85XYP215"] = {
+	    exhaust = "ta488f154",
+	    category = "car",
+	},
+	["45JSB406"] = {
+	    exhaust = "taaud40v8",
+	    category = "car",
+	},
 }
