8ceb6d24657e91d91e922b1fa2a3da851bfe9085
Annastasia <112778590+Annalouu@users.noreply.github.com>
2025-08-04 05:11:33 +0100

    Update location.lua

--- data/location.lua
+++ data/location.lua
@@ -1,12 +1,12 @@
 return {
-	["asetcm"] = {
+	{
 		coords = vector3(287.847626, -329.072266, 43.919857),
 		radius = 2.400000,
 		groups = nil,
 		debug = false,
 		drawtext = { inveh = "Press E to engineswap", outveh = "You need to be in a vehicle!" }
 	},
-	["ltxtnq"] = {
+	{
 		coords = vector3(294.708588, -342.779633, 43.919857),
 		radius = 2.600000,
 		groups = {["mechanic"] = 3,},
